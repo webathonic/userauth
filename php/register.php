242c8f053eb8c8931d1f7af627dcd270f6d48bef
Oyetunde Adeola <114435497+webathonic@users.noreply.github.com>
2022-10-24 20:37:24 +0100

    Update register.php

--- php/register.php
+++ php/register.php
@@ -9,17 +9,42 @@ registerUser($username, $email, $password);
 }
 
 function registerUser($username, $email, $password){
+    $user= array(
+        'fullname' => $username,
+        'email'=> $email,
+        'password' => $password,       
+    );
     //save data into the file
-    $folder= "../storage/users.csv";
-    $detail = fopen($folder, "ra+");    
-    fwrite($detail, "$username, $email, $password \n");
-    
-    fclose($detail);
+    if (checkUserExist(email)){
+        echo "User Already Exists";
+    }else{
+        $detail = fopen($folder, "ra+"); 
+        fputincsv($detail, $user);
+         fclose($detail);
     
     // echo "OKAY";
     echo "User Successfully registered";
+    }
+}
+    
+   
+    
+//now check if the user exist
+function checkUserExist($email){
+    $folder= "../storage/users.csv";
+    $detail = fopen($folder, "ra+"); 
+    while (!feof($detail)){
+        $line = fgetcsv($detail);
+        if ($line[1] == $email){
+            return true;
+   }
+ }
+        return false;   
 }
 ?>
+
+
+
 
 
 
