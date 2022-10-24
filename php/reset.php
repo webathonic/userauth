563c3afa38195b58f81ef7ea66eb26360ab37279
Oyetunde Adeola <114435497+webathonic@users.noreply.github.com>
2022-10-24 23:56:16 +0100

    Update reset.php

--- php/reset.php
+++ php/reset.php
@@ -1,15 +1,27 @@
 <?php
 if(isset($_POST['submit'])){
-    $email = //complete this;
-    $newpassword = //complete this;
+    $email = $_POST['email'];
+    $newpassword =$_POST['password'];
 
-    resetPassword($email, $password);
+    resetPassword($email, $newpassword);
 }
 
-function resetPassword($email, $password){
-    //open file and check if the username exist inside
-    //if it does, replace the password
+function resetPassword($email, $password){    
+    $folder= "../storage/users.csv";
+    $detail = fopen($folder, "r");     
+    while (!feof($detail)){
+        $line = fgetcsv($detail);
+        if ($line[1] == $email){
+            $line[2]=$password;
+            fclose($detail);
+            $detail = fopen($folder, "w");
+            fputcsv($detail,$line);
+            fclose($detail);
+            echo "Password reset successfully";
+            exit();  
+        }
+    }     
+    
 }
-echo "HANDLE THIS PAGE";
 
 
