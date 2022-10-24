eb317c42bf1a9f60eb6ebd42904754829592f39b
Oyetunde Adeola <114435497+webathonic@users.noreply.github.com>
2022-10-24 22:56:23 +0100

    Update login.php

--- php/login.php
+++ php/login.php
@@ -4,44 +4,23 @@ if (isset($_POST['submit'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    if (loginUser($email, $password)){
-        header('location:.../dashboard.php');
-    }else{
-        echo 'Incorrect Details';
-        echo '<meta http-equiv="refresh" content="2; url=../forms/login.html">';
-    }
+    loginUser($email, $password);
 }
 
 function loginUser($email, $password)
 {
-    $folder= "../storage/users.csv";
-    $detail = fopen($folder, "r"); 
+    $folder = "../storage/users.csv";
+    $detail = fopen($folder, "r");
 
-    while (!feof($detail)){
-        $line= fgetcsv($detail);
+    while (!feof($detail)) {
+        $line = fgetcsv($detail);
         if ($line[1] == $email && $line[2] == $password) {
             $_SESSION['username'] = $line[0];
             header("location:../dashboard.php");
-            exit()
-        }else{
-            echo '<script>
-            alert(" Sorry... Email or Password is incorrect");
-            window.location.href="../forms/logins.html";
-            </script>';
-        }
-
+            exit();
+        } else {
+            echo " Sorry... Email or Password is incorrect";
+            fclose($detail);
         }
     }
-
-    fclose($database);
-    if ($success=true){
-        session_start();
-        $_SESSION['email'] = $email;
-        $_SESSION['password']=$password;
-        session_write_close();
-        header("location:../dashboard.php");
-   
 }
-
-
-   
